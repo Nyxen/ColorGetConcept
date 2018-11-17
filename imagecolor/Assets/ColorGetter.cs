857ca2cae662bc186b5d48580d11412b8e0811c2
Isaac Argueta <blessthefallensouls@gmail.com>
2018-11-17 02:33:50 -0800

    Adding project files, could easily make it so mouse shows color of each pixel, could add if necessary/wanted

---
+++ imagecolor/Assets/ColorGetter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityScript.Scripting.Pipeline;
+
+
+public class ColorGetter : MonoBehaviour {
+
+	// Use this for initialization
+	private new SpriteRenderer renderer;
+	
+	public Text displayLabel;
+
+	void Start ()
+	{
+		renderer = GetComponent<SpriteRenderer>();
+		Color32[] data = renderer.sprite.texture.GetRawTextureData<Color32>().ToArray();
+		StringBuilder text = new StringBuilder();
+
+		
+		for (int i = 0; i < data.Length; i++)
+		{
+			
+			text.Append($"R:{data[i].r}, G:{data[i].g}, B:{data[i].b}");
+			text.Append("\n");
+		}
+
+		displayLabel.text += text.ToString();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
